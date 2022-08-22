ed6faff4de95d2d65723ca3902b9795c28e50e5a
Kamran Mushtaq <kamranmushtaq26@gmail.com>
2022-08-22 08:58:56 +0100

    Initial commit

--- build/js/main.js
+++ build/js/main.js
@@ -1,2 +1,17 @@
 /** test **/
+
+// select dropdown
+const govcyDropDown = (selector) => {
+  let element = document.querySelector(selector)
+  accessibleAutocomplete.enhanceSelectElement({
+    defaultValue: '',
+    showAllValues: true,
+    selectElement: element,
+    confirmOnBlur: false,
+    onConfirm: (a) =>
+      console.log(a)
+  })
+}
+
+// Mobile OTP
 govcyMobileOtp();
